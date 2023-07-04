a34657ddea411a49f9381f7b1247823c6fd04b34
gbsehnsucht <eatmeatdrinkbeer@yandex.ru>
2023-07-04 14:39:24 +0700

    add process_map

---
+++ process_map.py
@@ -0,0 +1,16 @@
+import folium as _f
+
+
+def create_map(city_geo, points, routes_to_nearest_node, routes_between_points):
+    m = _f.Map([city_geo[1], city_geo[0]])
+    for i in range(len(points)):
+        _f.Marker([points.geometry[i].y, points.geometry[i].x],
+                  popup=points.description[i], icon=_f.Icon(color='green')).add_to(m)
+    for route in routes_to_nearest_node:
+        _f.PolyLine(route, color='orange').add_to(m)
+    for route in routes_between_points:
+        _f.PolyLine(route, color='red').add_to(m)
+
+    m.save('.\\source\\maps\\test_map.html')
+    # m.save(input('Введите имя файла для сохранения карты маршрутов: '))
+
