2add252ec07d48d12c1951553caa4cb6a2936434
gbsehnsucht <eatmeatdrinkbeer@yandex.ru>
2023-08-08 18:10:08 +0700

    move functions to process_geo

--- main.py
+++ main.py
@@ -20,11 +20,8 @@ if 'venv' in sys.executable:
               5. Просмотреть файл geojson/osmxml.
               6. Просмотреть содержимое БД.
               7. Выйти из программы''')
-        try:
-            choice = int(input('Введите число от 1 до 7, обозначающее пункт меню: '))
-            return choice
-        except ValueError:
-            print('Некорректный ввод, требуется ввести число от 1 до 7!')
+        choice = int(input('Введите число от 1 до 7, обозначающее пункт меню: '))
+        return choice
 
 
     def get_file(source_dir):
@@ -78,21 +75,6 @@ if 'venv' in sys.executable:
                 not_found_menu()
 
 
-    def get_city():
-        city_variants, geo_variants = [], []
-        while not city_variants:
-            city_variants, geo_variants = find_city(input('Введите название города: '))
-            if not city_variants:
-                not_found_menu()
-        num = select_option(city_variants) if len(city_variants) > 1 else 0
-        return city_variants[num], geo_variants[num]
-
-
-    def translate_city(city_name):
-        with open('.\\source\\tdict.txt', 'r', encoding='utf-8') as tdict:
-            return eval(tdict.read())[city_name]
-
-
     def create_route_map(filename, city_name, city_geo, points):
         tsp_list = solve_tsp(points)
         print('Построение маршрутов...')
@@ -109,158 +91,167 @@ if 'venv' in sys.executable:
 
 
     def main():
-        choice = main_menu()
-        while choice != 7:
+        try:
+            choice = main_menu()
 
-            if choice == 1:
-                source = '.\\source\\xls\\'
-                try:
-                    working_file = source + get_file(source)
-                    city_name, city_geo = get_city()
+            while choice != 7:
 
-                    add_city(city_name, working_file)
-                    fill_coordinates(working_file)
-
-                    depot_address, depot_geo = manual_input(city_name, 'Введите ваш текущий адрес: ')
-                    address_list, geo_list = [depot_address], [depot_geo]
-                    addresses_from_file, geo_from_file = return_from_file(working_file)
-                    address_list.extend(addresses_from_file)
-                    geo_list.extend(geo_from_file)
-
-                    city_name = translate_city(city_name)
-                    save_graphml_to_file(city_name)
-
-                    filename = set_filename()
-
-                    if suggest_to_save('Сохранить список адресов в базу данных? [Д/Н]:'):
-                        insert_data(filename, address_list[1:], geo_list[1:])
-
-                    create_geo_files(filename, address_list, geo_list)
-
-                    points = load_geom(filename)
-
-                    create_route_map(filename, city_name, city_geo, points)
-
-
-                except ImportError:
-                    print_line()
-
-            elif choice == 2:
-                source = '.\\source\\geojson_osm\\'
-                try:
-                    working_file = get_file(source).split('.')[0]
-                    if f'{working_file}.osm' not in os.listdir('.\\source\\geojson_osm\\'):
-                        convert_to_osm(working_file)
-
-                    points = load_geom(working_file)
-
-                    city_name, city_geo = get_city()
-                    while not city_name + ',' in points.description[0]:
-                        print('Введенный город не совпадает с указанным в файле!')
-                        city_name, city_geo = get_city()
-                    depot_address, depot_geo = manual_input(city_name, 'Введите ваш текущий адрес: ')
-
-                    city_name = translate_city(city_name)
-                    save_graphml_to_file(city_name)
-
-                    points = change_depot(points, depot_address, depot_geo) if check_creator(working_file) \
-                        else add_depot(points, depot_address, depot_geo)
-
-                    create_route_map(filename, city_name, city_geo, points)
-
-                except ImportError:
-                    print_line()
-
-            elif choice == 3:
-
-                try:
-                    base, engine = connect_to_db()
-                    table = get_table(base)
-                    addresses_from_db, geo_from_db = get_data(table, base, engine)
-
-                    city_name, city_geo = get_city()
-                    while not city_name + ',' in addresses_from_db[0]:
-                        print('Введенный город не совпадает с указанным в базе данных!')
+                if choice == 1:
+                    source = '.\\source\\xls\\'
+                    try:
+                        working_file = source + get_file(source)
                         city_name, city_geo = get_city()
 
-                    depot_address, depot_geo = manual_input(city_name, 'Введите ваш текущий адрес: ')
-                    address_list, geo_list = [depot_address], [depot_geo]
-                    address_list.extend(addresses_from_db)
-                    geo_list.extend(geo_from_db)
+                        add_city(city_name, working_file)
+                        fill_coordinates(working_file)
 
-                    city_name = translate_city(city_name)
-                    save_graphml_to_file(city_name)
+                        depot_address, depot_geo = manual_input(city_name, 'Введите ваш текущий адрес: ')
+                        address_list, geo_list = [depot_address], [depot_geo]
+                        addresses_from_file, geo_from_file = return_from_file(working_file)
+                        address_list.extend(addresses_from_file)
+                        geo_list.extend(geo_from_file)
 
-                    filename = set_filename()
+                        city_name = translate_city(city_name)
+                        save_graphml_to_file(city_name)
 
-                    create_geo_files(filename, address_list, geo_list)
+                        filename = set_filename()
 
-                    points = load_geom(filename)
+                        if suggest_to_save('Сохранить список адресов в базу данных? [Д/Н]:'):
+                            insert_data(filename, address_list[1:], geo_list[1:])
 
-                    create_route_map(filename, city_name, city_geo, points)
+                        create_geo_files(filename, address_list, geo_list)
 
-                except ImportError:
-                    print_line()
+                        points = load_geom(filename)
 
-            elif choice == 4:
-                try:
-                    city_name, city_geo = get_city()
-                    city_name = translate_city(city_name)
-                    save_graphml_to_file(city_name)
-                    depot_address, depot_geo = manual_input(city_name, 'Введите ваш текущий адрес: ')
-                    address_list, geo_list = [depot_address], [depot_geo]
-                    while True:
-                        address, geo = manual_input(city_name, 'Введите адрес объекта: ')
-                        address_list.append(address)
-                        geo_list.append(geo)
-                        repeat_menu('Добавить еще один адрес? [Д/Н]:')
-                except ImportError:
-                    print_line()
-                except AssertionError:
-                    filename = set_filename()
+                        create_route_map(filename, city_name, city_geo, points)
 
-                    if suggest_to_save('Сохранить список адресов в базу данных? [Д/Н]:'):
-                        insert_data(filename, address_list[1:], geo_list[1:])
 
-                    create_geo_files(filename, address_list, geo_list)
+                    except ImportError:
+                        print_line()
 
-                    points = load_geom(filename)
-
-                    create_route_map(filename, city_name, city_geo, points)
-
-            elif choice == 5:
-                source = '.\\source\\geojson_osm\\'
-                try:
-                    while True:
+                elif choice == 2:
+                    source = '.\\source\\geojson_osm\\'
+                    try:
                         working_file = get_file(source).split('.')[0]
                         if f'{working_file}.osm' not in os.listdir('.\\source\\geojson_osm\\'):
                             convert_to_osm(working_file)
-                        points = load_geom(working_file)
-                        print(points)
-                        print_line()
-                        repeat_menu('Просмотреть другой файл? [Д/Н]:')
-                except (ImportError, AssertionError):
-                    print_line()
 
-            elif choice == 6:
-                try:
-                    while True:
+                        points = load_geom(working_file)
+
+                        city_name, city_geo = get_city()
+                        while not city_name + ',' in points.description[0]:
+                            print('Введенный город не совпадает с указанным в файле!')
+                            city_name, city_geo = get_city()
+
+                        depot_address, depot_geo = manual_input(city_name, 'Введите ваш текущий адрес: ')
+
+                        city_name = translate_city(city_name)
+                        save_graphml_to_file(city_name)
+
+                        points = change_depot(points, depot_address, depot_geo) if check_creator(working_file) \
+                            else add_depot(points, depot_address, depot_geo)
+
+                        create_route_map(filename, city_name, city_geo, points)
+
+                    except ImportError:
+                        print_line()
+
+                elif choice == 3:
+
+                    try:
                         base, engine = connect_to_db()
                         table = get_table(base)
-                        address_list, geo_list = get_data(table, base, engine)
-                        filename = 'temp'
-                        create_geojson(filename, address_list, geo_list)
-                        convert_to_osm(filename)
-                        points = load_geom(filename)
-                        print(points)
-                        os.remove('.\\source\\geojson_osm\\temp.geojson')
-                        os.remove('.\\source\\geojson_osm\\temp.osm')
-                        print_line()
-                        repeat_menu('Просмотреть другую таблицу? [Д/Н]:')
-                except (ImportError, AssertionError):
-                    print_line()
+                        addresses_from_db, geo_from_db = get_data(table, base, engine)
 
-            choice = main_menu()
+                        city_name, city_geo = get_city()
+                        while not city_name + ',' in addresses_from_db[0]:
+                            print('Введенный город не совпадает с указанным в базе данных!')
+                            city_name, city_geo = get_city()
+
+                        depot_address, depot_geo = manual_input(city_name, 'Введите ваш текущий адрес: ')
+                        address_list, geo_list = [depot_address], [depot_geo]
+                        address_list.extend(addresses_from_db)
+                        geo_list.extend(geo_from_db)
+
+                        city_name = translate_city(city_name)
+                        save_graphml_to_file(city_name)
+
+                        filename = set_filename()
+
+                        create_geo_files(filename, address_list, geo_list)
+
+                        points = load_geom(filename)
+
+                        create_route_map(filename, city_name, city_geo, points)
+
+                    except ImportError:
+                        print_line()
+
+                elif choice == 4:
+                    try:
+                        city_name, city_geo = get_city()
+                        city_name = translate_city(city_name)
+                        save_graphml_to_file(city_name)
+                        depot_address, depot_geo = manual_input(city_name, 'Введите ваш текущий адрес: ')
+                        address_list, geo_list = [depot_address], [depot_geo]
+                        while True:
+                            address, geo = manual_input(city_name, 'Введите адрес объекта: ')
+                            address_list.append(address)
+                            geo_list.append(geo)
+                            repeat_menu('Добавить еще один адрес? [Д/Н]:')
+                    except ImportError:
+                        print_line()
+                    except AssertionError:
+                        filename = set_filename()
+
+                        if suggest_to_save('Сохранить список адресов в базу данных? [Д/Н]:'):
+                            insert_data(filename, address_list[1:], geo_list[1:])
+
+                        create_geo_files(filename, address_list, geo_list)
+
+                        points = load_geom(filename)
+
+                        create_route_map(filename, city_name, city_geo, points)
+
+                elif choice == 5:
+                    source = '.\\source\\geojson_osm\\'
+                    try:
+                        while True:
+                            working_file = get_file(source).split('.')[0]
+                            if f'{working_file}.osm' not in os.listdir('.\\source\\geojson_osm\\'):
+                                convert_to_osm(working_file)
+                            points = load_geom(working_file)
+                            print(points)
+                            print_line()
+                            repeat_menu('Просмотреть другой файл? [Д/Н]:')
+                    except (ImportError, AssertionError):
+                        print_line()
+
+                elif choice == 6:
+                    try:
+                        while True:
+                            base, engine = connect_to_db()
+                            table = get_table(base)
+                            address_list, geo_list = get_data(table, base, engine)
+                            filename = 'temp'
+                            create_geojson(filename, address_list, geo_list)
+                            convert_to_osm(filename)
+                            points = load_geom(filename)
+                            print(points)
+                            os.remove('.\\source\\geojson_osm\\temp.geojson')
+                            os.remove('.\\source\\geojson_osm\\temp.osm')
+                            print_line()
+                            repeat_menu('Просмотреть другую таблицу? [Д/Н]:')
+                    except (ImportError, AssertionError):
+                        print_line()
+
+                else:
+                    raise ValueError
+
+        except ValueError:
+            print('Некорректный ввод, требуется ввести число от 1 до 7!')
+            # choice = main_menu()
+
         close_dadata_socket()
 
     main()
--- process_geo.py
+++ process_geo.py
@@ -35,6 +35,21 @@ def select_option(options_list):
             print('Некорректный ввод!')
 
 
+def get_city():
+    city_variants, geo_variants = [], []
+    while not city_variants:
+        city_variants, geo_variants = find_city(input('Введите название города: '))
+        if not city_variants:
+            not_found_menu()
+    num = select_option(city_variants) if len(city_variants) > 1 else 0
+    return city_variants[num], geo_variants[num]
+
+
+def translate_city(city_name):
+    with open('.\\source\\tdict.txt', 'r', encoding='utf-8') as tdict:
+        return json.loads(tdict.read()).get(city_name)
+
+
 def find_city(city):
     data = dadata.suggest('address', city)
     city_variants, geo_c_variants = [], []
